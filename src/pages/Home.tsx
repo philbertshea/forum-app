1209967a0a2448a117993ff268f9ba83e3e4b19c
Philbert Shea <philbertshea@gmail.com>
2025-05-17 22:44:33 +0800

    Add Login and Register buttons to Home
    Home page looks quite dull.
    
    Let's
    * Add the login and register buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {Button} from "@mui/material";
 
 function Home() {
-    return <h1>
-        Welcome to our page!
-    </h1>;
+    return (
+        <div>
+            <h1>Welcome to the forum!</h1>
+            <Button variant="outlined" onClick={() => window.location.href = "/login"}>Log In</Button>
+            <h3>If you do not have an account, register here.</h3>
+            <Button variant="outlined" onClick={() => window.location.href = "/register"}>Register</Button>
+        </div>
+    );
 }
 
 export default Home;
